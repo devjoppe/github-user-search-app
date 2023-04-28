f03ad1c837ff239af54a38528130e84c0f4ee73c
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-04-28 16:33:14 +0200

    Style the links section, and now the base of mobile view is done

--- src/components/UserMainLinks.tsx
+++ src/components/UserMainLinks.tsx
@@ -2,12 +2,23 @@ import {profileInterface} from "../interfaces/profileInterface.tsx";
 
 const UserMainLinks = ({profileData}:profileInterface) => {
     return(
-        <div>
-            <p>User links</p>
-            <span>{profileData.location}</span>
-            <span>{profileData.blog}</span>
-            <span>{profileData.twitter_username}</span>
-            <span>{profileData.company}</span>
+        <div className="main-profile-links">
+            <div>
+                <img src="src/assets/icons/icon-location.svg" alt="Location" />
+                <span>{profileData.location}</span>
+            </div>
+            <div>
+                <img src="src/assets/icons/icon-website.svg" alt="Location" />
+                <span>{profileData.blog}</span>
+            </div>
+            <div>
+                <img src="src/assets/icons/icon-twitter.svg" alt="Location" />
+                <span>{profileData.twitter_username}</span>
+            </div>
+            <div>
+                <img src="src/assets/icons/icon-company.svg" alt="Location" />
+                <span>{profileData.company}</span>
+            </div>
         </div>
     )
 }
