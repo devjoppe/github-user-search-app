cfd9901dbdb51432d8c7aca1f36c1b6b684df8b5
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-04-28 16:18:15 +0200

    styled the profile stats

--- src/components/UserMainStats.tsx
+++ src/components/UserMainStats.tsx
@@ -2,13 +2,19 @@ import {profileInterface} from "../interfaces/profileInterface.tsx";
 
 const UserMainStats = ({profileData}:profileInterface) => {
     return(
-        <div>
-            <span>Repos</span>
-            <span>{profileData.public_repos}</span>
-            <span>Followers</span>
-            <span>{profileData.followers}</span>
-            <span>Following</span>
-            <span>{profileData.following}</span>
+        <div className="main-profile-stats">
+            <div>
+                <span className="title">Repos</span>
+                <span className="number">{profileData.public_repos}</span>
+            </div>
+            <div>
+                <span className="title">Followers</span>
+                <span className="number">{profileData.followers}</span>
+            </div>
+            <div>
+                <span className="title">Following</span>
+                <span className="number">{profileData.following}</span>
+            </div>
         </div>
     )
 }
