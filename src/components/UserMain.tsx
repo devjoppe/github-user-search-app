6bfe0edf05311d7734b4bf1cb003344e30c46a65
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-04-28 15:59:16 +0200

    Fixed placement of avatar. Fixed the date data to match the design and styling of the bio description. All in both theme modes

--- src/components/UserMain.tsx
+++ src/components/UserMain.tsx
@@ -5,16 +5,20 @@ import {profileInterface} from "../interfaces/profileInterface.tsx";
 import UserMainLinks from "./UserMainLinks.tsx";
 import UserLeft from "./UserLeft.tsx";
 const UserMain = ({profileData}:profileInterface) => {
-
     return(
-        <div>
-            <UserLeft profileData={profileData}/>
-            <div>
+        <>
+            <div className="main-profile-top">
+                <UserLeft profileData={profileData}/>
                 <UserMainProfile profileData={profileData}/>
+            </div>
+            <div className="main-profile-bottom">
+                <div className="main-profile-bio">
+                    <p>{profileData.bio}</p>
+                </div>
                 <UserMainStats profileData={profileData}/>
                 <UserMainLinks profileData={profileData}/>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/components/UserMainProfile.tsx
+++ src/components/UserMainProfile.tsx
@@ -1,14 +1,25 @@
 import {profileInterface} from "../interfaces/profileInterface.tsx";
+import {useEffect, useState} from "react";
 
 const UserMainProfile = ({profileData}:profileInterface) => {
+
+    const [joinedDate, setJoinedDate] = useState('')
+
+    useEffect(() => {
+        if(profileData.created_at) {
+            const date = new Date(profileData.created_at)
+            const day = date.getUTCDay()
+            const month = new Intl.DateTimeFormat("en-US", {month: "short"}).format(date)
+            const year = date.getUTCFullYear()
+            setJoinedDate(`${day} ${month} ${year}`)
+        }
+    }, [profileData.created_at])
+
     return(
-        <div>
+        <div className="main-profile-top-content">
             <h2>{profileData.name}</h2>
-            <span>{profileData.login}</span>
-            <p>{profileData.bio}</p>
-            <div>
-                <span>{profileData.created_at}</span>
-            </div>
+            <span className="login">@{profileData.login}</span>
+            <span>Joined {joinedDate}</span>
         </div>
     )
 }
