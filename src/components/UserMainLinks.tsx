a3d51d949548cf792294e97c8ea5e94a535f33d6
Joakim Ottosson <joakim.ottosson@medieinstitutet.se>
2023-04-29 12:02:26 +0200

    Added function to display Not available in correct color and then fixed spacing i the list

--- src/components/UserMainLinks.tsx
+++ src/components/UserMainLinks.tsx
@@ -1,23 +1,57 @@
 import {profileInterface} from "../interfaces/profileInterface.tsx";
 
 const UserMainLinks = ({profileData}:profileInterface) => {
+
     return(
         <div className="main-profile-links">
             <div>
-                <img src="src/assets/icons/icon-location.svg" alt="Location" />
-                <span>{(profileData.location ? profileData.location : 'Not available')}</span>
+                {(profileData.location ?
+                    <>
+                        <img className="isData" src="src/assets/icons/icon-location.svg" alt="Location" />
+                        <span className="isData">{profileData.location}</span>
+                    </> :
+                    <>
+                        <img className="none" src="src/assets/icons/icon-location.svg" alt="Location" />
+                        <span className="none">Not available</span></>
+                )}
             </div>
             <div>
-                <img src="src/assets/icons/icon-website.svg" alt="Location" />
-                <span>{(profileData.blog ? profileData.blog : 'Not available')}</span>
+                {(profileData.blog ?
+                    <>
+                        <img className="isData" src="src/assets/icons/icon-website.svg" alt="Location" />
+                        <a href={profileData.blog}>
+                            <span className="isData">{profileData.blog}</span>
+                        </a>
+                    </> :
+                    <>
+                        <img className="none" src="src/assets/icons/icon-website.svg" alt="Location" />
+                        <span className="none">Not available</span>
+                    </>
+                )}
             </div>
             <div>
-                <img src="src/assets/icons/icon-twitter.svg" alt="Location" />
-                <span>{(profileData.twitter_username ? profileData.twitter_username : 'Not available')}</span>
+                {(profileData.twitter_username ?
+                    <>
+                        <img className="isData" src="src/assets/icons/icon-twitter.svg" alt="Location" />
+                        <span className="isData">{profileData.blog}</span>
+                    </> :
+                    <>
+                        <img className="none" src="src/assets/icons/icon-twitter.svg" alt="Location" />
+                        <span className="none">Not available</span>
+                    </>
+                )}
             </div>
             <div>
-                <img src="src/assets/icons/icon-company.svg" alt="Location" />
-                <span>{(profileData.company ? profileData.company : 'Not available')}</span>
+                {(profileData.company ?
+                        <>
+                            <img className="isData" src="src/assets/icons/icon-company.svg" alt="Location" />
+                            <span className="isData">{profileData.company}</span>
+                        </> :
+                        <>
+                            <img className="none" src="src/assets/icons/icon-company.svg" alt="Location" />
+                            <span className="none">Not available</span>
+                        </>
+                )}
             </div>
         </div>
     )
